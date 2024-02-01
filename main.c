1cef776fa9ae8aaf36cb6841b245af5e86f3aeca
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-02-01 15:13:26 +0100

    map parsing working. more tests necessary

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include "cub3D.h"
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-	t_map map;
+	t_data	image;
+	t_asset	assets;
+	t_map	map;
+
+	memset(&assets, 0, sizeof(assets));
+	memset(&map, 0, sizeof(map));
+	image.exit_loop = false;
+	image.map = &map;
+	image.a = &assets;
 	
 	map_check(argc, argv, &map);
+	printf("%s", map.buffer);
+	printf("Player direction: %c\n", map.player_direction);
 	return(0);
 }
